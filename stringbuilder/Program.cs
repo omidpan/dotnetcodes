db1345a7bdaf4d35bbe8ce76a78d509fa815d426
leopn <omidpnteaching@gmail.com>
2025-09-19 12:01:05 -0400

    string builder

---
+++ stringbuilder/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/**
+ In C#, strings are immutable.
+
+Every time you modify a string (e.g., using + concatenation), a new string object is created
+in memory.
+
+This is fine for small operations, but if you’re building or modifying text repeatedly
+(e.g., loops, large files), it becomes slow and memory-wasteful.
+ 
+ */
+namespace stringbuilder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "";
+            var start1 = DateTime.Now;
+            for (int i = 0; i < 10000; i++)
+            {
+                result += i.ToString() + " ";  // Each loop creates a new string!
+            }
+            var end1 = DateTime.Now;
+            Console.WriteLine("String time: " + (end1 - start1).TotalMilliseconds);
+            Console.WriteLine(result);
+            /******************** 
+             StringBuilder (in System.Text) is a mutable string class.
+             It allows you to modify text in place (append, insert, remove, replace) 
+            without creating a new string every time.Much faster for repeated modifications.
+             **********************************/
+
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Hello"); // Append text.
+            sb.Append(" World");
+            sb.AppendLine("!");//Adds text and a newline.
+            sb.Insert(6, "C# ");//Insert text at a specific position.
+            sb.Replace("World", "Universe");
+            sb.Remove(6, 6);  // Remove 6 chars from index 6(remove " Univer")
+
+            Console.WriteLine(sb.ToString()); // Output: C# Universe!
+
+            sb.Clear(); // Clear all content.
+            Console.WriteLine("After Clear: '" + sb.ToString() + "'");
+            /*
+             * Let's compare the time taken by StringBuilder vs string concatenation in a loop.
+             */
+            var start2 = DateTime.Now;
+            for (int i = 0; i < 10000; i++)
+                sb.Append(i);
+            var end2 = DateTime.Now;
+            Console.WriteLine("StringBuilder time: " + (end2 - start2).TotalMilliseconds);
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ stringbuilder/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("stringbuilder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Champlain College")]
+[assembly: AssemblyProduct("stringbuilder")]
+[assembly: AssemblyCopyright("Copyright © Champlain College 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2aff7045-3794-4b9b-bfa2-7f4021716b69")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
