d8bbc50f403b465640bfadfd65812e3087e7e7dd
leopn <omidpnteaching@gmail.com>
2025-11-05 17:36:37 -0500

    yeald added

---
+++ collections-and-reflections/BookFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collections_and_reflections
+{
+    public class BookFactory
+    {
+        public static Book CreateBook(string title, string author, int yearPublished)
+        {
+            Book book= new Book();
+            book.Title = title;
+            book.Author = author;
+            book.YearPublished = yearPublished;
+            return book;
+        }
+    }
+}
